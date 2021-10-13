0c66002ab0486d952c3d3a9923ca6dddd8726b1d
hzx <huozhixin.hzx@alibaba-inc.com>
2021-10-13 16:08:16 +0800

    add all api

--- providers/example-provider.go
+++ providers/example-provider.go
@@ -9,6 +9,7 @@ import (
 	"github.com/virtual-kubelet/virtual-kubelet/pkg/config"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"strconv"
 	"strings"
 )
@@ -26,6 +27,8 @@ type TestProvider struct {
 	internalIP         	string
 	daemonEndpointPort 	int32
 	PodDataMap			map[string][]string
+	PodLabelMap			map[string]map[string]string
+	PodUIDMap			map[string]string
 }
 
 func NewTestProvider(rm *manager.ResourceManager, nodeName, operatingSystem string, internalIP string, daemonEndpointPort int32) (*TestProvider, error) {
@@ -49,8 +52,16 @@ func (provider *TestProvider) CreatePod(ctx context.Context, pod *v1.Pod) error 
 	if provider.PodDataMap == nil {
 		provider.PodDataMap = make(map[string][]string)
 	}
+	if provider.PodUIDMap == nil{
+		provider.PodUIDMap = make(map[string]string)
+	}
+	if provider.PodLabelMap == nil{
+		provider.PodLabelMap = make(map[string]map[string]string)
+	}
 	podName := provider.getPodName(pod.Name, pod.Namespace)
 	provider.PodDataMap[podName] = make([]string, 0)
+	provider.PodUIDMap[podName] = string(pod.ObjectMeta.UID)
+	provider.PodLabelMap[podName] = pod.Labels
 	for _, container := range provider.getContainers(pod){
 		containerID, err := provider.client.CreateDockerContainer(ctx, container.PodName, container.PodNamespace, container.ContainerName, container.Image)
 		if err != nil {
@@ -74,6 +85,8 @@ func (provider *TestProvider) DeletePod(ctx context.Context, pod *v1.Pod) error 
 
 func (provider *TestProvider) UpdatePod(ctx context.Context, pod *v1.Pod) error {
 	labels := pod.Labels
+	podName := provider.getPodName(pod.Name, pod.Namespace)
+	provider.PodLabelMap[podName] = pod.Labels
 	if labels["state"] == "checkpoint" {
 		containers, err := reserveContainersArray(provider.getContainers(pod))
 		if err != nil {
@@ -126,24 +139,91 @@ func (provider *TestProvider) GetPodStatus(ctx context.Context, namespace, name 
 	return &pod.Status, nil
 }
 
-func (provider *TestProvider) GetPods(context.Context) ([]*v1.Pod, error) {
+func (provider *TestProvider) GetPods(ctx context.Context) ([]*v1.Pod, error) {
 	results := make([]*v1.Pod, len(provider.PodDataMap))
 	for key, containers := range provider.PodDataMap {
 		namespace, name := provider.getPodMessage(key)
+		k8sContainers, k8sContainersStatus, err := provider.convertContainerToK8SContainers(ctx , containers)
+		if err != nil {
+			return nil, err
+		}
 		results = append(results, &v1.Pod{
 			TypeMeta: metav1.TypeMeta{
-
+				Kind:       "Pod",
+				APIVersion: "v1",
 			},
 			ObjectMeta: metav1.ObjectMeta{
 				Name: name,
 				Namespace: namespace,
-
+				ClusterName: provider.clusterName,
+				UID: types.UID(provider.PodUIDMap[key]),
+			},
+			Spec: v1.PodSpec{
+				NodeName: provider.nodeName,
+				Volumes: []v1.Volume{},
+				Containers: k8sContainers,
+			},
+			Status: v1.PodStatus{
+				Phase: "Running",
+				Conditions: []v1.PodCondition{
+					v1.PodCondition{
+						Type:               v1.PodReady,
+						Status:             v1.ConditionTrue,
+						LastTransitionTime: metav1.Time{},
+					}, v1.PodCondition{
+						Type:               v1.PodInitialized,
+						Status:             v1.ConditionTrue,
+						LastTransitionTime: metav1.Time{},
+					}, v1.PodCondition{
+						Type:               v1.PodScheduled,
+						Status:             v1.ConditionTrue,
+						LastTransitionTime: metav1.Time{},
+					},
+				},
+				Message: "",
+				Reason: "",
+				HostIP: "",
+				PodIP: provider.internalIP,
+				ContainerStatuses: k8sContainersStatus,
 			},
 		})
 	}
 
 	return nil, nil
 }
+
+
+
+func(provider *TestProvider) convertContainerToK8SContainers(context context.Context, containers []string) ([]v1.Container, []v1.ContainerStatus, error) {
+	resultContainers := make([]v1.Container, len(containers))
+	resultContainersStatus := make([]v1.ContainerStatus, len(containers))
+	for _, containerID := range containers {
+		dockerContainer, err := provider.client.GetContainerMessage(context, containerID)
+		if err != nil {
+			return nil, nil, err
+		}
+		resultContainers = append(resultContainers, v1.Container{
+			Name: dockerContainer.ID,
+			Image: dockerContainer.Image,
+			Command: []string{dockerContainer.Command},
+			Resources: v1.ResourceRequirements{
+				Requests: v1.ResourceList{},
+			},
+		})
+		resultContainersStatus = append(resultContainersStatus, v1.ContainerStatus{
+			Name: dockerContainer.ID,
+			ContainerID: dockerContainer.ID,
+			Image: dockerContainer.Image,
+			ImageID: dockerContainer.ImageID,
+			Ready: true,
+		})
+	}
+	return resultContainers, resultContainersStatus, nil
+}
+
+//func containerStateToPodPhase(state string) v1.PodPhase {
+//	return ""
+//}
 
 func reserveContainersArray(containers []ContainerData) ([]ContainerData, error) {
 	length := len(containers)
